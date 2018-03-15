1e26edd96b54824d58cc65a1a03aa2adc4f19665
Adriaan Moors <adriaan@lightbend.com>
2018-03-15 23:14:17 +0100

    html-norm

--- html-norm.sh
+++ html-norm.sh
@@ -2,6 +2,6 @@
 
 # TODO: not entirely working yet, tidy fails hard when html can't be parsed
 # also, the output is not entirely canonical
-tidy -q --sort-attributes alpha --wrap 0 -i -utf8  -ashtml "$1"  2> /dev/null
+cat "$1" | perl -pe 's/\s+/ /gm' | tidy -q --sort-attributes alpha --wrap 0 -i -utf8  -ashtml - 2> /dev/null
 
 exit 0
