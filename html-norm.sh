1e51f399e7ad792982fdd3d43c4572e796288032
Adriaan Moors <adriaan@lightbend.com>
2018-03-15 17:30:20 +0100

    tidy can sort attributes

--- html-norm.sh
+++ html-norm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # TODO: not entirely working yet, tidy fails hard when html can't be parsed
-# xsltproc is last because tidy seems to not preserve attrib order, but we want them sorted for smaller diffs
 # also, the output is not entirely canonical
-tidy -q -wrap 0 -i -utf8  -ashtml "$1" 2> /dev/null | xsltproc --html ~/bin/xml-attr-sort.xml 2> /dev/null
+tidy -q --sort-attributes alpha --wrap 0 -i -utf8  -ashtml "$1"  2> /dev/null
 
 exit 0
